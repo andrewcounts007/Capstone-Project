187c1e35895359723437c0ab87111f30f94f0ea7
Andrew Counts <andrewcounts007@gmail.com>
2021-08-22 19:58:35 -0500

    Add Empty Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MainComponent } from './main/main.component';
+import { AdminComponent } from './admin/admin.component';
+import { ItemsComponent } from './items/items.component';
+import { PurchaseComponent } from './purchase/purchase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent,
+    AdminComponent,
+    ItemsComponent,
+    PurchaseComponent
   ],
   imports: [
     BrowserModule,
