7c189938a189c72296672e1e62e8449e7325a81d
Andrew Counts <andrewcounts007@gmail.com>
2021-08-22 20:18:24 -0500

    App-routing for login, register, and user-profile componants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile/:id', component: UserProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
