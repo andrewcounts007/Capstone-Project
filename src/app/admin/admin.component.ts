da7b36ca979f04a5770905311c6404f1b044a9a9
Andrew Counts <andrewcounts007@gmail.com>
2021-09-05 12:44:14 -0500

    front end design is complete, mongo user database is functioning

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -5,11 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
+
 export class AdminComponent implements OnInit {
+
+  post() {
+    let boxnumber = document.getElementsByName('inlineRadioOptions');
+    let newTitle = document.getElementById('title')?.textContent;
+
+    console.log("hello");
+    console.log(newTitle);
+    alert('hey');
+
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
 }
+
+
+
+
+
+
+
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MainComponent } from './main/main.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'profile/:id', component: UserProfileComponent }
+  { path: 'profile/:id', component: UserProfileComponent },
+  { path: 'main', component: MainComponent },
+  { path: 'admin', component: AdminComponent }
 ];
 
 @NgModule({
